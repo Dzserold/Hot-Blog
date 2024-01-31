4cf5fe449a984f7b9c49b50ddf2cab32d4d208f1
Dzserold <f.dzserold@gmail.com>
2024-01-31 19:27:20 +0100

    adding state to search bar

--- src/lib/component/Search.tsx
+++ src/lib/component/Search.tsx
@@ -1,6 +1,25 @@
+"use client";
+import { useState } from "react";
+
 const Search = () => {
+  const [search, setSearch] = useState("");
+
+  function updateSearch(e: string) {
+    setSearch(e);
+    console.log(search);
+  }
+
   return (
-    <input className="custom_input" placeholder="Search posts" type="text" />
+    <div>
+      <input
+        className="custom_input"
+        placeholder="Search posts"
+        type="text"
+        value={search}
+        onChange={(e) => updateSearch(e.target.value)}
+      />
+      <h1>{search}</h1>
+    </div>
   );
 };
 
